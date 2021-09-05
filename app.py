ea1e021e499de36ee1e22743e37f9b6f8747c977
Bryce Beckenbach <brycebeckenbach@gmail.com>
2021-09-05 14:20:58 -0400

    add environ variables for heroku

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ import os
 ##############################################################################
 # Configurations
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///wealth_and_budget_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('database', 'postgresql:///wealth_and_budget_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', os.getenv('SECRET_KEY'))
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 ##############################################################################
 # Initializations
