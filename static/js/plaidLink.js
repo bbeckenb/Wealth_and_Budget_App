cde8f33a2b67dcafda68203b02ce61dade100190
Bryce Beckenbach <brycebeckenbach@gmail.com>
2021-11-10 13:05:11 -0500

    updated front end ufi account addition functionality

--- static/js/plaidLink.js
+++ static/js/plaidLink.js
@@ -4,20 +4,23 @@
         onLoad: function() {
         },
         onSuccess: (async function(public_token, metadata) {
-            try {
-                startLoadScreen();
-                const newUfi = await $.post('/exchange_public_token', {
-                    public_token: public_token,
-                })
-                const newUFIwithAccounts = await $.post(`/financial-institutions/${newUfi.id}/accounts/add`);
-                addUFItoPage(newUFIwithAccounts);
-                addAccountsToUFI(newUFIwithAccounts.accounts, newUFIwithAccounts.id);
-                updateDashboardBalances(newUFIwithAccounts.dashboardBalanceNoLoan, newUFIwithAccounts.dashboardBalanceWithLoan);
-                endLoadScreen();
-                addAlert(newUFIwithAccounts.message);
-            } catch (err) {
-                console.error('Server problem connecting with Plaid:', err)
+            async function createUFIandAccounts() {
+                try {
+                    startLoadScreen();
+                    const newUfi = await $.post('/exchange_public_token', {
+                        public_token: public_token,
+                    })
+                    const newUFIwithAccounts = await $.post(`/financial-institutions/${newUfi.id}/accounts/add`);
+                    addUFItoPage(newUFIwithAccounts);
+                    addAccountsToUFI(newUFIwithAccounts.accounts, newUFIwithAccounts.id);
+                    updateDashboardBalances(newUFIwithAccounts.dashboardBalanceNoLoan, newUFIwithAccounts.dashboardBalanceWithLoan);
+                    endLoadScreen();
+                    addAlert(newUFIwithAccounts.message);
+                } catch (err) {
+                    console.error('Server problem connecting with Plaid:', err)
+                }
             }
+        createUFIandAccounts();
        
         }),
         onExit: function(err, metadata) {
